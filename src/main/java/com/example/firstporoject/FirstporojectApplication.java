1f0f18023ad34b4c6ca37a43b57b796f338653c6
SOGA <carte8@naver.com>
2023-01-12 01:57:13 +0900

    Initial commit

---
+++ src/main/java/com/example/firstporoject/FirstporojectApplication.java
@@ -0,0 +1,14 @@
+package com.example.firstporoject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstporojectApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FirstporojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/firstporoject/FirstporojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstporoject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstporojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
